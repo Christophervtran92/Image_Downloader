2bb2378d346809b8810cd5766cd7b8cc3a861759
Christophervtran92 <christophervtran92@gmail.com>
2022-03-30 17:18:02 -0700

    Changed name to just Image Downloader to prevent potential for people to think this is created by Tweepy themselves.

--- Image_Downloader.py
+++ Image_Downloader.py
@@ -1,6 +1,30 @@
-# Tweepy Image Downloader by Christopher Tran
+# Image Downloader by Christopher Tran
 # 03/23/2022
+
+# Tweepy License Information: 
 #
+# MIT License
+# Copyright (c) 2009-2022 Joshua Roesslein
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 # A Python application designed to download images from twitter tweets. Currently, being used
 # with a personal twitter account that is mainly used to transfer photos from different 
 # applications to my home computer. This app is designed to simplify the process and avoid the
@@ -18,7 +42,7 @@ tokens = json.load(open('tokens.json', 'r')) #Import tokens from JSON
 auth = tweepy.OAuth1UserHandler(
     tokens["api_Key"], tokens["api_Key_Secret"], tokens["access_Token"], tokens["access_Token_Secret"]
 )
-api = tweepy.API(auth)  #Ready the  API
+api = tweepy.API(auth)  #Ready the API
 tweets_Timeline = api.user_timeline(count=10)   #Grab the 10 latest tweets
 
 #Print list of 10 most recent tweets
