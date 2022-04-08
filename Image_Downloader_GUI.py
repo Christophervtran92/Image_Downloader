626391a2a7ac937afc05f205bd040f99de6a6bda
Christophervtran92 <christophervtran92@gmail.com>
2022-04-08 15:23:42 -0700

    Updated style of GUI a bit, updated code to have a more consistent style

--- Image_Downloader_GUI.py
+++ Image_Downloader_GUI.py
@@ -3,6 +3,7 @@ import requests #For downloading images
 import json     #For importing Tokens from JSON
 from tkinter import *   #For GUI
 from tkinter import messagebox
+from tkinter import font
 from PIL import ImageTk, Image  #For images
 from io import BytesIO
 from winreg import *
@@ -16,8 +17,8 @@ from winreg import *
 def download(info):
     # From: https://www.reddit.com/r/learnpython/comments/4dfh1i/how_to_get_the_downloads_folder_path_on_windows/
     # Get user Download folder for saving
-    with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders') as key:
-        location = QueryValueEx(key, '{374DE290-123F-4565-9164-39C4925E467B}')[0]
+    with OpenKey(HKEY_CURRENT_USER, "SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders") as key:
+        location = QueryValueEx(key, "{374DE290-123F-4565-9164-39C4925E467B}")[0]
     filename = info["id"]
     extension = ".jpg"
     save_Location = location + "\\" + filename + extension
@@ -25,7 +26,7 @@ def download(info):
     media_Url_Lg = truncated_Url + "?format=jpg&name=4096x4096"
     print("File: " + save_Location)
     img_Data = requests.get(media_Url_Lg).content
-    with open(save_Location, 'wb') as handler:
+    with open(save_Location, "wb") as handler:
         handler.write(img_Data)
     download_Info = ("\nFilename:\n" + filename + extension +
                     "\n\nSave Location:\n" + save_Location)
@@ -37,8 +38,9 @@ def download(info):
 def gui(tokens):
     #Create the window for the application
     window = Tk(screenName="Image Downloader", baseName=None, className="Image Downloader", useTk=1)
-    window.geometry('1000x900')
+    window.geometry("1200x900")
     window.title("Image Downloader")
+    window.resizable(False, False)
 
     #Authenticate the twitter account with info from tokens.json
     auth = tweepy.OAuth1UserHandler(
@@ -53,7 +55,7 @@ def gui(tokens):
         tempHashtag = api.get_status(tweet.id).entities["hashtags"]
         hashtagArray = []
         for hashtag in tempHashtag:
-            hashtagArray.append(hashtag['text'])
+            hashtagArray.append(hashtag["text"])
         url = str(api.get_status(tweet.id).entities["media"][0]["media_url"])
         maxsize = (600, 600)
         imageURL = requests.get(url)
@@ -61,7 +63,7 @@ def gui(tokens):
         picture.thumbnail(maxsize, Image.Resampling.LANCZOS)
 
         #Store id, url, time created, and thumbnail into a JSON for current tweet and append to detailsArray
-        infoJSON = {"id": str(api.get_status(tweet.id).entities["media"][0]['id']),
+        infoJSON = {"id": str(api.get_status(tweet.id).entities["media"][0]["id"]),
                     "url": str(api.get_status(tweet.id).entities["media"][0]["media_url"]),
                     "time":  api.get_status(tweet.id).created_at.ctime(),
                     "hashtags": hashtagArray,
@@ -87,8 +89,8 @@ def gui(tokens):
     frame = Frame(window, width=700, height=850)
     frame.pack(expand=True, fill=BOTH)
 
-    #scroll region (approx height of frames) * len(thumbnails) where thumbnails array of images
-    canvas = Canvas (frame, bg="#1da1f2", width=700, height=850, scrollregion=(0,0,900, 371 * len(detailsArray)))
+    #scroll region (approx height of frames) * len(detailsArray) where len(detailsArray) is number of images
+    canvas = Canvas (frame, bg="#1da1f2", width=700, height=850, scrollregion=(0,0,900, 392 * len(detailsArray)))
 
     #Create a vertical scrollbar and place it on the RHS of frame, bind it to scroll canvas vertically
     scrollbar = Scrollbar(frame, orient=VERTICAL)
@@ -100,18 +102,19 @@ def gui(tokens):
     canvas.config(yscrollcommand=scrollbar.set)
     canvas.pack(expand=True, side=LEFT, fill=BOTH)
 
-    padding = 190   #Increase default padding for top most frame
+
+    padding = 210   #Increase default padding for top most frame
     for details in detailsArray:
-        infoFrame = LabelFrame(text=" " + details["url"] + " ", font=("Arial", 12)) #Parent frame for L&R frames
+        infoFrame = LabelFrame(text=" ID: " + details["id"] + " ", font=("Arial", 12, font.BOLD)) #Parent frame for L&R frames
         leftFrame = Frame(infoFrame)    #lhs frame to hold thumbnail image
-        leftFrame.pack(side = LEFT)     #place it on the lhs of infoFrame
+        leftFrame.pack(side = LEFT, padx=5, pady=15)     #place it on the lhs of infoFrame
         rightFrame = Frame(infoFrame)   #rhs frame to hold information and dl button
-        rightFrame.pack(side = RIGHT)   #place it on the rhs of infoFrame
-        label = Label(infoFrame, image=details["thumbnail"])   #Label to display the thumbnail image
+        rightFrame.pack(side = RIGHT, padx=5, pady=15)   #place it on the rhs of infoFrame
+        label = Label(infoFrame, image=details["thumbnail"], relief="solid")   #Label to display the thumbnail image
         label.pack(side = LEFT)                     #Place it on lhs of infoFrame
-        infoBox = Text(rightFrame, width=35, height=16, padx=5)
-        description = ( "Id:\n" + details['id'] +
-                            "\n\nCreated on:\n" + details['time'] +
+        infoBox = Text(rightFrame, width=35, height=16, padx=5, relief="solid")
+        description = ( "Url:\n" + details["url"] +
+                            "\n\nCreated on:\n" + details["time"] +
                             "\n\nHashtags: " + str(details["hashtags"]))
         infoBox.insert(END, description)
         infoBox.config(state="disabled")
@@ -119,11 +122,11 @@ def gui(tokens):
 
         #Info for making unique buttons on each loop found here: 
         #https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
-        downloadBtn = Button(rightFrame, text="Download", width=20, height=2, command= lambda details=details: download(details, window))
-        downloadBtn.pack(side = BOTTOM, anchor="s", pady=15) #Placed at bottom of RHS of infoFrame, padding away from infoBox
-        canvas.create_window(475, padding, window=infoFrame) #first number adjusts the lhs padding for each window
-        padding += 370  #Adjust padding increment as needed to add spacing between frames
+        downloadBtn = Button(rightFrame, text="Download", width=20, height=2, command= lambda details=details: download(details))
+        downloadBtn.pack(side = BOTTOM, anchor='s', pady=15) #Placed at bottom of RHS of infoFrame, padding away from infoBox
+        canvas.create_window(580, padding, window=infoFrame) #first number adjusts the lhs padding for each window
+        padding += 390  #Adjust padding increment as needed to add spacing between frames
 
     window.mainloop()
 
-gui(json.load(open('tokens.json', 'r')))
+gui(json.load(open("tokens.json", 'r')))
